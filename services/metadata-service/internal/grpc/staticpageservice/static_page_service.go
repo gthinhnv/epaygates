fc9a12ec9d92e81ea999a598d6ccc52d076ac834
Thinh Nguyen <thinhnv.vnu@gmail.com>
2025-11-29 06:06:34 +0700

    t

--- services/metadata-service/internal/grpc/staticpageservice/static_page_service.go
+++ services/metadata-service/internal/grpc/staticpageservice/static_page_service.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"fmt"
 	"metadatasvc/gen/go/staticpagepb"
+
+	"buf.build/go/protovalidate"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type StaticPageServiceServer struct {
@@ -16,5 +20,8 @@ func NewStaticPageServiceServer() *StaticPageServiceServer {
 
 func (s *StaticPageServiceServer) Create(ctx context.Context, req *staticpagepb.CreateRequest) (*staticpagepb.CreateResponse, error) {
 	fmt.Println("____________Create a new static page______________")
+	if err := protovalidate.Validate(req); err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "validation failed: %v", err)
+	}
 	return &staticpagepb.CreateResponse{}, nil
 }
