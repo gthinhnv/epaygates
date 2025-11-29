9fab2929503895311b2f2c1ac00e052fd42f3f73
Thinh Nguyen <thinhnv.vnu@gmail.com>
2025-11-29 10:20:21 +0700

    t

--- services/metadata-service/internal/grpc/staticpageservice/static_page_service.go
+++ services/metadata-service/internal/grpc/staticpageservice/static_page_service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"metadatasvc/gen/go/staticpagepb"
+	"metadatasvc/internal/bootstrap"
 
 	"buf.build/go/protovalidate"
 	"google.golang.org/grpc/codes"
@@ -21,7 +22,22 @@ func NewStaticPageServiceServer() *StaticPageServiceServer {
 func (s *StaticPageServiceServer) Create(ctx context.Context, req *staticpagepb.CreateRequest) (*staticpagepb.CreateResponse, error) {
 	fmt.Println("____________Create a new static page______________")
 	if err := protovalidate.Validate(req); err != nil {
+		if ve, ok := err.(*protovalidate.ValidationError); ok {
+			for _, violation := range ve.Violations {
+				fmt.Println("fieldName", protovalidate.FieldPathString(violation.Proto.GetField()))
+				fmt.Println("message", violation.Proto.GetMessage())
+				fmt.Println("ruleId", violation.Proto.GetRuleId())
+			}
+		}
 		return nil, status.Errorf(codes.InvalidArgument, "validation failed: %v", err)
 	}
-	return &staticpagepb.CreateResponse{}, nil
+
+	id, err := bootstrap.Repos.StaticPageRepo.Create(ctx, req)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create static page: %v", err)
+	}
+
+	return &staticpagepb.CreateResponse{
+		Id: id,
+	}, nil
 }
