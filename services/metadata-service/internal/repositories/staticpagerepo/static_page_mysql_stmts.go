68fb2e5a1cd68bc83796177dca7bbe16ea6b161a
Thinh Nguyen <thinhnv.vnu@gmail.com>
2025-12-02 16:35:22 +0700

    t

--- services/metadata-service/internal/repositories/staticpagerepo/static_page_mysql_stmts.go
+++ services/metadata-service/internal/repositories/staticpagerepo/static_page_mysql_stmts.go
@@ -5,29 +5,11 @@ import (
 )
 
 var createStmt *sqlx.Stmt
-var updateStmt *sqlx.Stmt
-var deleteStmt *sqlx.Stmt
-var getByIDStmt *sqlx.Stmt
 
 func prepareStatements(db *sqlx.DB) error {
 	var err error
 
 	createStmt, err = prepareCreateStatement(db)
-	if err != nil {
-		return err
-	}
-
-	updateStmt, err = prepareUpdateStatement(db)
-	if err != nil {
-		return err
-	}
-
-	deleteStmt, err = prepareDeleteStatement(db)
-	if err != nil {
-		return err
-	}
-
-	getByIDStmt, err = prepareGetByIDStatement(db)
 	if err != nil {
 		return err
 	}
@@ -52,47 +34,6 @@ func prepareCreateStatement(db *sqlx.DB) (*sqlx.Stmt, error) {
             created_at,
             updated_at
         ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW())
-    `
-	return db.Preparex(query)
-}
-
-func prepareUpdateStatement(db *sqlx.DB) (*sqlx.Stmt, error) {
-	const query = `
-        SELECT
-			id,
-            title,
-            slug,
-            content,
-            page_type,
-            sort_order,
-            ads_platform,
-            status,
-            created_by,
-            updated_by,
-            created_at,
-            updated_at,
-            deleted_version
-		FROM static_pages
-        WHERE id = ? AND deleted_version = 0
-        LIMIT 1
-    `
-	return db.Preparex(query)
-}
-
-func prepareDeleteStatement(db *sqlx.DB) (*sqlx.Stmt, error) {
-	const query = `
-        SELECT * FROM static_pages
-        WHERE id = ? AND deleted_version = 0
-        LIMIT 1
-    `
-	return db.Preparex(query)
-}
-
-func prepareGetByIDStatement(db *sqlx.DB) (*sqlx.Stmt, error) {
-	const query = `
-        SELECT * FROM static_pages
-        WHERE id = ? AND deleted_version = 0
-        LIMIT 1
     `
 	return db.Preparex(query)
 }
